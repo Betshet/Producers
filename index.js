599d93f515f31d8b3a8c4d2ed3de724094d0ef8f
Betshet <45632828+Betshet@users.noreply.github.com>
2019-12-13 14:58:34 +0100

    Adding files

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var three = require('three');
+var events = require('events');
+var mysql = require('mysql');
+var path    = require("path");
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('Data'));
+
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+var types = [
+  {
+    title: "producteurs",
+    description: "Execute C++ executable as a child process, using command " +
+                 "line args and stdout.  Based on /cpp/standalone_stdio"
+  }
+];
+
+
+var server = app.listen(3000, function () {
+  console.log('Web server listing at http://localhost:%s', server.address().port);
+  console.log(server);
+});
+
+app.get( '/', function (req,res) {
+	console.log('hi');
+	return res;
+});
